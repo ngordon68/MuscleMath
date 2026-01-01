20da89582cc47cec99a3a3b8d1a3ede8fab40317
Ngordon68 <ngordon688@gmail.com>
2026-01-01 13:00:05 -0500

    Initial Commit

---
+++ MuscleMath/MuscleMathApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuscleMathApp.swift
+//  MuscleMath
+//
+//  Created by Nick Gordon on 1/1/26.
+//
+
+import SwiftUI
+
+@main
+struct MuscleMathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
